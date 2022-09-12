21155f60eb26d07b38ef52dc29b2908b159ba108
Denniely Garcia <denniely.garcia09@fmail.com>
2022-09-12 16:22:12 +0800

    activity 2: To find the Full Name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def find_fullname
+
+  name = { first_name: "daniel", middle_name: "juan", last_name: "dela cruz" }
+  puts "#{name[:first_name]}"
+  puts "#{name[:middle_name]}"
+  puts "#{name[:last_name]}"
+end
+
+
