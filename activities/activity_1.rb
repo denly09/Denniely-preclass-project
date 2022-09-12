107e7afdf92ab4b5714b160fd6323654d36473ba
Denniely Garcia <denniely.garcia09@fmail.com>
2022-09-12 15:38:33 +0800

    activity 4: to find the age of the legal age

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,20 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def who_is_adult(ages)
+
+  adults = []
+
+  ages.each do |age|
+
+    if age >= 18
+
+      adults << age
+    end
+  end
+
+  return adults
+end
+
+p who_is_adult([16, 30, 11, 19, 10, 25])
