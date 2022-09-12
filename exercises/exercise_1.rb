c159b40c481c44ec387e27a8d1d185aee33fc1d1
Denniely Garcia <denniely.garcia09@fmail.com>
2022-09-12 14:52:02 +0800

    exercise 1: create script tp calculate bmi

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,11 @@
-# TODO: ACTIVITY 1
-#  Write a function that takes an Array of ages,
-#  the function will return the average age of adults.
+# TODO: EXERCISE 1
+#  BMI is weight in kilograms divided by the result of height in meters raised to 2.
+#  Write a function that calculates BMI.
 #  Print on console the result.
 
-def
+def calculate_bmi(weight, height)
+  bmi = weight / height**2
+  return bmi
+end
+
+p calculate_bmi(65, 1.75)
